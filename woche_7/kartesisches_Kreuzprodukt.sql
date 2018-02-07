a9646633a339a81710bc119c87d2045fbc6b2339
Karl Steinam <webmaster@steinam.de>
2018-02-07 14:26:05 +0100

    Fragen zu group und aggregatfunktion

---
+++ woche_7/kartesisches_Kreuzprodukt.sql
@@ -0,0 +1,27 @@
+﻿
+-- KOmbination aller Datensätze de rlinken Tabell emit allen DS der rechtne Tabelle
+-- kartesiches Kreuzprodukt
+select *
+from spieler, strafen
+where spieler.spielernr = strafen.spielernr
+
+
+
+select datum, betrag, `sp`.`SPIELERNR`, st.`SPIELERNR`
+from spieler sp, strafen st
+where sp.spielernr = st.spielernr
+
+-- zeige mir alle spieler und wenn sie strafen haben auch noch den Betrag und das Datum
+
+select datum, betrag, `sp`.`SPIELERNR`, st.`SPIELERNR`
+from spieler sp  left join strafen st
+	on sp.`SPIELERNR` = st.`SPIELERNR`
+
+
+
+
+
+select * from strafen
+
+
+select 112 / 8
